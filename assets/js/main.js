b147a9620d1b58961dc036ff3890c50f101116fa
Chris Reynolds (acn) <christopher.reynolds@accenture.com>
2015-10-23 14:39:19 +0100

    Initial waether site commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,17 @@
 /*=> Main Javascript
 --------------------------------------------------*/
 
+var weather = {
+    apiKey: '906c85fb2efd2af0991f7f14391563eb'
+};
 
+weather.api = function() {
+    
+}
+
+
+(function($) {
+
+
+
+})(jQuery || window.jQuery);
