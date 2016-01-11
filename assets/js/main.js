150c7a3af9e4c0fb47638ad69491a195dcd93154
Chris Reynolds <christopher.reynolds@accenture.com>
2016-01-11 17:51:03 +0000

    Resetting assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,6 @@
 /*=> Main Javascript
 --------------------------------------------------*/
 
-var weather = {
-    apiKey: '906c85fb2efd2af0991f7f14391563eb'
-};
-
-weather.api = function() {
-    
-}
-
-
 (function($) {
 
 
