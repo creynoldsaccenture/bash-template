6cc75db65a7372705a51b9671a548051a0e48548
Chris Reynolds <christopher.reynolds@accenture.com>
2016-01-13 15:09:03 +0000

    Add Github email prompt and convert CRLF to LF (Unix)

--- configure_vm.sh
+++ configure_vm.sh
@@ -1,53 +1,67 @@
 #!usr/bin/bash
 
-# Variables
-git_email="christopher.reynolds@accenture.com"
+# Prompt user for their Github emaill address (required for setting up SSH keys)
+echo -e "\nPlease enter your Github email address:"
+read git_email
 
-# Install Git, Ruby, Node, NPM and Grunt 
-sudo apt-get install git
-sudo apt-get install ruby
-sudo apt-get install nodejs-legacy
-sudo apt-get install npm
-npm install grunt --save-dev
+if "$git_email" !== ""; then
+    initial_setup
+else
+    echo -e "\nThis script requires your Github email address to perform initial set up."
+    exit 1
+fi
 
-# Check that Ruby is installed
-ruby -v
+sub initial_setup {
 
-# Set up Git aliases
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.cm commit
-git config --global alias.st status
-git config --global alias.un 'reset HEAD --'
+    # Install Git, Ruby, Node, NPM and Grunt 
+    sudo apt-get install git
+    sudo apt-get install ruby
+    sudo apt-get install nodejs-legacy
+    sudo apt-get install npm
+    npm install grunt --save-dev
 
-# Set up SSH keys
-ssh-keygen -t rsa -b 4096 -C "$git_email"
-ssh-agent -s
-ssh-add ~/.ssh/id_rsa
+    # Check that Ruby is installed
+    ruby -v
 
-echo "Copy this SSH key and paste it into the SSH keys section of your Github profile\n"
-cat ~/.ssh/id_rsa.pub
-echo "\n\n"
+    # Set up Git aliases
+    git config --global alias.co checkout
+    git config --global alias.br branch
+    git config --global alias.cm commit
+    git config --global alias.st status
+    git config --global alias.un 'reset HEAD --'
 
-# Make dev folder
-cd ~/
-mkdir dev
-cd dev
+    # Set up SSH keys
+    ssh-keygen -t rsa -b 4096 -C "$git_email"
+    ssh-agent -s
+    ssh-add ~/.ssh/id_rsa
 
-# Clone the bash script repo
-git clone git@github.com:creynolds86/blank-project.git ./bash-template
+    echo "Copy this SSH key and paste it into the SSH keys section of your Github profile\n"
+    cat ~/.ssh/id_rsa.pub
+    echo "\n\n"
 
-# Checkout the bash template
-cd bash-template
-git co bash-template
+    # Make dev folder
+    cd ~/
+    mkdir dev
+    cd dev
+}
 
-# Install repo dependencies and build the app
-npm install
-grunt prod
+sub setup_bash_template {
 
-# Build the HTML file to serve
-chmod 755 ./system_info.sh
-./system_info.sh
+    # Clone the bash script repo
+    git clone git@github.com:creynolds86/blank-project.git ./bash-template
+
+    # Checkout the bash template
+    cd bash-template
+    git co bash-template
+
+    # Install repo dependencies and build the app
+    npm install
+    grunt prod
+
+    # Build the HTML file to serve
+    chmod 755 ./system_info.sh
+    ./system_info.sh
+}
 
 # Start the app server
 node server.js
